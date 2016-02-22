3a8f41c3dd79824efd81f3cd5f6b68a46155db80
macdg <ma_c_dg@hotmail.com>
2016-02-22 13:05:40 -0600

    Lo estoy borrando para subir la version final de este bloque sin logs, que son tambien archivos basura.

--- ci/application/logs/log-2016-01-27.php
+++
@@ -1,4 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2016-01-27 12:51:29 --> Severity: 4096 --> Object of class CI_Loader could not be converted to string /var/www/html/final/ci/application/controllers/Up_xml_c.php 219
-ERROR - 2016-01-27 13:08:23 --> Severity: 4096 --> Object of class CI_Loader could not be converted to string /var/www/html/final/ci/application/controllers/Up_xml.php 217
